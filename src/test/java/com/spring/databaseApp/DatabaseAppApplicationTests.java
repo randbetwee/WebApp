3ef7d6713b0783d148ed23b32064184f15d3483f
Yufeng Guan <112810750+randbetwee@users.noreply.github.com>
2023-12-03 10:32:58 +0800

    init twice
    add mybatis dependency

--- src/test/java/com/spring/databaseApp/DatabaseAppApplicationTests.java
+++ src/test/java/com/spring/databaseApp/DatabaseAppApplicationTests.java
@@ -1,10 +1,10 @@
-package com.spring.project;
+package com.spring.databaseApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectApplicationTests {
+class DatabaseAppApplicationTests {
 
 	@Test
 	void contextLoads() {
