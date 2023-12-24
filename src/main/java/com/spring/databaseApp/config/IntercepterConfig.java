fb5f2acf56487f2fad93a1d6719a71f7e99977eb
Weiqi Luo <wikii3@outlook.com>
2023-12-24 17:14:06 +0800

    v1.0.2 fix address and cert

--- src/main/java/com/spring/databaseApp/config/IntercepterConfig.java
+++ src/main/java/com/spring/databaseApp/config/IntercepterConfig.java
@@ -35,7 +35,7 @@ public class IntercepterConfig implements WebMvcConfigurer{
                 //是否发送Cookie
                 .allowCredentials(true)
                 //放行哪些原始域
-                .allowedOriginPatterns("https://localhost*", "https://8.134.198.180*", "https://www.cold-front.cn*")
+                .allowedOriginPatterns("https://localhost*", "https://20.24.102.235*", "https://www.cold-front.top*", "https://cold-front.top*")
                 .allowedMethods(new String[]{"OPTIONS","GET", "POST", "PUT", "DELETE"})
                 .allowedHeaders("*")
                 .exposedHeaders("*");
